623627630a6142ef3acabd102d68d16971708e92
rgsk <guptarahul70322@gmail.com>
2025-07-27 14:29:19 +0530

    home

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,5 +1,11 @@
 interface HomePageProps {}
 const HomePage: React.FC<HomePageProps> = ({}) => {
-  return <div>HomePage</div>;
+  return (
+    <div className="flex flex-col justify-center items-center py-[200px]">
+      <h1 className="text-5xl">Skartner</h1>
+      <div className="h-[20px]"></div>
+      <h2 className="text-3xl">Your Partner in Skills Development</h2>
+    </div>
+  );
 };
 export default HomePage;
