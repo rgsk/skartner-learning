89701e477f2d5ed8e466923db9f4ab2261d2e9ca
rgsk <guptarahul70322@gmail.com>
2025-07-27 17:54:46 +0530

    sidebar open by default

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,14 +1,13 @@
 import { GlobalContextProvider } from "@/app/providers/GlobalContextProvider";
 import { ThemeProvider } from "@/app/providers/ThemeProvider";
 import { SidebarProvider } from "@/components/ui/sidebar";
-import { cookies } from "next/headers";
 
 interface ProvidersProps {
   children: any;
 }
 const Providers: React.FC<ProvidersProps> = async ({ children }) => {
-  const cookieStore = await cookies();
-  const defaultOpen = cookieStore.get("sidebar_state")?.value === "true";
+  // const cookieStore = await cookies();
+  // const defaultOpen = cookieStore.get("sidebar_state")?.value === "true";
 
   return (
     <ThemeProvider
@@ -18,7 +17,7 @@ const Providers: React.FC<ProvidersProps> = async ({ children }) => {
       disableTransitionOnChange
     >
       <GlobalContextProvider>
-        <SidebarProvider defaultOpen={defaultOpen}>{children}</SidebarProvider>
+        <SidebarProvider defaultOpen={true}>{children}</SidebarProvider>
       </GlobalContextProvider>
     </ThemeProvider>
   );
