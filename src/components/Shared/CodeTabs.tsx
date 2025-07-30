db059720f41c233f38f6adfdd82103ea49dce0c8
rgsk <guptarahul70322@gmail.com>
2025-07-30 15:51:50 +0530

    preferred tab

--- src/components/Shared/CodeTabs.tsx
+++ src/components/Shared/CodeTabs.tsx
@@ -1,4 +1,6 @@
+"use client";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { useLocalStorage } from "@uidotdev/usehooks";
 import SyntaxHighlighter from "../Shared/SyntaxHighlighter";
 
 interface CodeTabsProps {
@@ -12,9 +14,17 @@ interface CodeTabsProps {
   };
 }
 const CodeTabs: React.FC<CodeTabsProps> = ({ python, cpp }) => {
+  const [preferredTab, setPreferredTab] = useLocalStorage<"Python" | "C++">(
+    "preferredTab",
+    "Python"
+  );
+
   return (
     <div className="my-[20px]">
-      <Tabs defaultValue="Python">
+      <Tabs
+        value={preferredTab}
+        onValueChange={(v) => setPreferredTab(v as any)}
+      >
         <TabsList>
           <TabsTrigger value="Python">Python</TabsTrigger>
           {cpp && <TabsTrigger value="C++">C++</TabsTrigger>}
