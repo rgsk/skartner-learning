e61fd93197e88298301935eb2991810973955340
rgsk <guptarahul70322@gmail.com>
2025-08-01 02:19:29 +0530

    fix

--- src/components/DSA/Searching/BinarySearchConcept.tsx
+++ src/components/DSA/Searching/BinarySearchConcept.tsx
@@ -8,7 +8,7 @@ const BinarySearchConcept: React.FC<BinarySearchConceptProps> = ({}) => {
   return (
     <div>
       <Heading>Binary Search Concept</Heading>
-      <PracticeLinks leetcode="binary-search" workattech="contains-element" />
+      <PracticeLinks leetcode="binary-search" />
       <CodeFetcher
         pythonFile="/dsa/searching/binary_search_concept/sol/binary_search.py"
         cppFile="/dsa/searching/binary_search_concept/sol/binary_search.cpp"
