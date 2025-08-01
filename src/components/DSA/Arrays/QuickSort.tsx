2a219bbaf75482ad0c6a82a176ae8d091181adab
rgsk <guptarahul70322@gmail.com>
2025-08-01 17:17:05 +0530

    fix

--- src/components/DSA/Arrays/QuickSort.tsx
+++ src/components/DSA/Arrays/QuickSort.tsx
@@ -1,22 +1,83 @@
 "use client";
 import CodeFetcher from "@/components/Shared/CodeFetcher";
+import Container from "@/components/Shared/Container";
+import Examples from "@/components/Shared/Examples";
 import Heading from "@/components/Shared/Heading";
+import MinorHeading from "@/components/Shared/MinorHeading";
+import Paragraph from "@/components/Shared/Paragraph";
 import PracticeLinks from "@/components/Shared/PracticeLinks";
+import Problem from "@/components/Shared/Problem";
+import Section from "@/components/Shared/Section";
+import Solution from "@/components/Shared/Solution";
+import SubHeading from "@/components/Shared/SubHeading";
+import UnorderedList from "@/components/Shared/UnorderedList";
 
 interface QuickSortProps {}
 const QuickSort: React.FC<QuickSortProps> = ({}) => {
   return (
-    <div>
+    <Container>
       <Heading>Implement Quicksort</Heading>
       <PracticeLinks
         leetcode="sort-an-array"
         workattech="implement-quicksort"
       />
-      <CodeFetcher
-        pythonFile="/dsa/arrays/quick_sort/sol/quick_sort.py"
-        cppFile="/dsa/arrays/quick_sort/sol/quick_sort.cpp"
-      />
-    </div>
+      <Problem>
+        <Section>
+          <SubHeading>Problem Statement</SubHeading>
+          <Paragraph>Given an array, sort it using quick sort.</Paragraph>
+        </Section>
+        <Section>
+          <MinorHeading>Examples</MinorHeading>
+          <Examples
+            items={[
+              `\
+            Input: nums = [5,2,3,1]
+            Output: [1,2,3,5]
+            `,
+            ]}
+          />
+        </Section>
+      </Problem>
+      <Solution>
+        <Section>
+          <SubHeading>Approach</SubHeading>
+          <Paragraph>Quicksort is a divide and conquer algorithm.</Paragraph>
+          <UnorderedList
+            items={[
+              "The basic idea is to divide the array into two parts about an element(say pivot point) such that the pivoted element is at the same position as the sorted array and all elements before the pivot point are less than the pivoted element and all elements after the pivot point are greater than or equal to the pivoted element.",
+              "Both the parts will be called recursively and the above process will be repeated. In this way, the array will break further until each partition becomes sorted.",
+            ]}
+          />
+          <Paragraph>
+            Note: Here, the last element of the partition is used as the pivoted
+            element.
+          </Paragraph>
+        </Section>
+        <Section>
+          <SubHeading>Analysis</SubHeading>
+          <UnorderedList
+            items={[
+              {
+                text: "Time Complexity",
+                children: [
+                  "Best Case: O(n * log(n))",
+                  "Average Case: O(n * log(n))",
+                  "Worst Case: O(n2)",
+                ],
+              },
+              "Auxiliary Space Complexity: O(log(n))",
+            ]}
+          />
+        </Section>
+        <Section>
+          <SubHeading>Implementation</SubHeading>
+          <CodeFetcher
+            pythonFile="/dsa/arrays/quick_sort/sol/quick_sort.py"
+            cppFile="/dsa/arrays/quick_sort/sol/quick_sort.cpp"
+          />
+        </Section>
+      </Solution>
+    </Container>
   );
 };
 export default QuickSort;
--- src/components/Shared/UnorderedList.tsx
+++ src/components/Shared/UnorderedList.tsx
@@ -1,13 +1,31 @@
+import React from "react";
+
 export interface UnorderedListProps {
-  items: string[];
+  items: (string | { text: string; children?: UnorderedListProps["items"] })[];
+}
+
+function renderItem(
+  item: string | { text: string; children?: UnorderedListProps["items"] },
+  idx: number
+): React.ReactNode {
+  if (typeof item === "string") {
+    return <li key={idx} dangerouslySetInnerHTML={{ __html: item }} />;
+  } else {
+    return (
+      <li key={idx}>
+        <span dangerouslySetInnerHTML={{ __html: item.text }} />
+        {item.children && item.children.length > 0 && (
+          <ul className="list-disc pl-5 space-y-2">
+            {item.children.map((child, childIdx) =>
+              renderItem(child, childIdx)
+            )}
+          </ul>
+        )}
+      </li>
+    );
+  }
 }
 
 export default function UnorderedList({ items }: UnorderedListProps) {
-  return (
-    <ul className={`list-disc pl-5 space-y-2`}>
-      {items.map((item, idx) => (
-        <li key={idx} dangerouslySetInnerHTML={{ __html: item }}></li>
-      ))}
-    </ul>
-  );
+  return <ul className="list-disc pl-5 space-y-2">{items.map(renderItem)}</ul>;
 }
