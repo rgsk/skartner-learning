4f8e8603ba335903d49cc73549c20aafbf07567d
rgsk <guptarahul70322@gmail.com>
2025-10-07 16:56:33 +0530

    fix

--- public/cses/introductory-problems/missing-number/xor-approach/missing-number.py
+++ public/cses/introductory-problems/missing-number/xor-approach/missing-number.py
@@ -1,22 +1,20 @@
 # setup-start
-def setup():
-    import os
-    import sys
-    script_directory = os.path.dirname(os.path.realpath(__file__))
-    parent_directory = os.path.dirname(script_directory)
-    input_path = os.path.join(parent_directory, 'input.txt')
-    output_path = os.path.join(parent_directory, 'output.txt')
-    if os.path.exists(input_path):
-        sys.stdin = open(input_path, 'r')
-        try:
-            sys.stdout = open(output_path, 'w')
-        except Exception as e:
-            pass
+import os
+import sys
+script_directory = os.path.dirname(os.path.realpath(__file__))
+parent_directory = os.path.dirname(script_directory)
+input_path = os.path.join(parent_directory, 'input.txt')
+output_path = os.path.join(parent_directory, 'output.txt')
+if os.path.exists(input_path):
+    sys.stdin = open(input_path, 'r')
+    try:
+        sys.stdout = open(output_path, 'w')
+    except Exception as e:
+        pass
 # setup-end
 
 
 def main():
-    setup()
     n = int(input())
     elements = [int(v) for v in input().split()]
     acc = 0
--- public/cses/introductory-problems/weird-algorithm/approach/weird-algorithm.py
+++ public/cses/introductory-problems/weird-algorithm/approach/weird-algorithm.py
@@ -1,20 +1,16 @@
 # setup-start
-def setup():
-    import os
-    import sys
-    script_directory = os.path.dirname(os.path.realpath(__file__))
-    parent_directory = os.path.dirname(script_directory)
-    input_path = os.path.join(parent_directory, 'input.txt')
-    output_path = os.path.join(parent_directory, 'output.txt')
-    if os.path.exists(input_path):
-        sys.stdin = open(input_path, 'r')
-        try:
-            sys.stdout = open(output_path, 'w')
-        except Exception as e:
-            pass
-
-
-setup()
+import os
+import sys
+script_directory = os.path.dirname(os.path.realpath(__file__))
+parent_directory = os.path.dirname(script_directory)
+input_path = os.path.join(parent_directory, 'input.txt')
+output_path = os.path.join(parent_directory, 'output.txt')
+if os.path.exists(input_path):
+    sys.stdin = open(input_path, 'r')
+    try:
+        sys.stdout = open(output_path, 'w')
+    except Exception as e:
+        pass
 # setup-end
 
 
