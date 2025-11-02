cda2bce3f5a9e09b875a1e3f3d2d1b1f2d9d5179
rgsk <guptarahul70322@gmail.com>
2025-11-02 13:36:55 +0530

    fix

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,18 +3,27 @@ import Navbar from "@/components/Navbar/Navbar";
 import { AppSidebar } from "@/components/Sidebars/AppSidebar";
 import { ControlsSidebar } from "@/components/Sidebars/ControlsSidebar";
 import useGlobalContext from "@/hooks/useGlobalContext";
+import useMeasure from "react-use-measure";
 
 interface AppProps {
   children: any;
 }
 const App: React.FC<AppProps> = ({ children }) => {
   const { showAppSidebar, showControlsSidebar } = useGlobalContext();
+  const [navbarContainerRef, navbarContainerBounds] = useMeasure();
   return (
     <div className="flex w-full">
       {showAppSidebar && <AppSidebar />}
-      <div className="flex-1 min-w-0">
-        <Navbar />
-        <main>{children}</main>
+      <div className="flex-1 min-w-0 relative overflow-hidden">
+        <div className="absolute top-0 w-full" ref={navbarContainerRef}>
+          <Navbar />
+        </div>
+        <div
+          className="absolute h-full w-full overflow-auto"
+          style={{ top: navbarContainerBounds.height }}
+        >
+          <main>{children}</main>
+        </div>
       </div>
       {showControlsSidebar && <ControlsSidebar />}
     </div>
