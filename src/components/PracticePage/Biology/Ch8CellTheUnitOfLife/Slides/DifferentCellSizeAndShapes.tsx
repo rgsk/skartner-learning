287e3d14c15c9870847e104b5cbc7d38d516fd1f
rgsk <guptarahul70322@gmail.com>
2026-02-16 12:17:28 +0530

    fix

--- src/components/PracticePage/Biology/Ch8CellTheUnitOfLife/Slides/DifferentCellSizeAndShapes.tsx
+++ src/components/PracticePage/Biology/Ch8CellTheUnitOfLife/Slides/DifferentCellSizeAndShapes.tsx
@@ -4,7 +4,7 @@ const DifferentCellSizeAndShapes = () => {
       <h3 className="text-2xl">Different Cell Sizes and Shapes</h3>
       <div className="h-4"></div>
       <div className="space-y-2">
-        <p>Cells differ greatly in size, shape and activities.</p>
+        <p>Cells differ greatly in size, shape and activities (Figure 8.1).</p>
         <p>
           For example, Mycoplasmas, the smallest cells, are only 0.3 mm in
           length while bacteria could be 3 to 5 µm.
@@ -21,6 +21,17 @@ const DifferentCellSizeAndShapes = () => {
           even irregular.
         </p>
         <p>The shape of the cell may vary with the function they perform.</p>
+      </div>
+      <div className="h-8"></div>
+      <div className="flex flex-col items-center gap-4">
+        <img
+          src="/biology/different-shapes-of-cells.png"
+          alt="different shapes of cells"
+          className="h-[500px]"
+        />
+        <p className="">
+          Figure 8.1 Diagram showing different shapes of the cells
+        </p>
       </div>
     </div>
   );
