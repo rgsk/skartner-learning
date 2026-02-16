4b9893405daf407e9e1a10952cf985324c1896be
rgsk <guptarahul70322@gmail.com>
2026-02-16 16:04:10 +0530

    fix

---
+++ src/components/PracticePage/Biology/Ch8CellTheUnitOfLife/Slides/Cytoskeleton.tsx
@@ -0,0 +1,22 @@
+const Cytoskeleton = () => {
+  return (
+    <div>
+      <h3 className="text-2xl text-blue-500">8.5.7 Cytoskeleton</h3>
+      <div className="h-4"></div>
+      <div className="space-y-2">
+        <p>
+          An elaborate network of filamentous proteinaceous structures
+          consisting of microtubules, microfilaments and intermediate filaments
+          present in the cytoplasm is collectively referred to as the
+          cytoskeleton.
+        </p>
+        <p>
+          The cytoskeleton in a cell are involved in many functions such as
+          mechanical support, motility, maintenance of the shape of the cell.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Cytoskeleton;
