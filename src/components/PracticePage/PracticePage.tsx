79bc2def05bc02d444d3897c7ab905211aeb6267
rgsk <guptarahul70322@gmail.com>
2026-04-01 11:13:05 +0530

    trying to write down physics

--- src/components/PracticePage/PracticePage.tsx
+++ src/components/PracticePage/PracticePage.tsx
@@ -1,10 +1,44 @@
 "use client";
 
+import SimpleLatex from "../Shared/SimpleLatex";
+
 interface PracticePageProps {}
 const PracticePage: React.FC<PracticePageProps> = ({}) => {
   return (
     <div className="p-[32px]">
-      <p>practice</p>
+      <div className="flex justify-center gap-[40px]">
+        <div>
+          <p>Scalar</p>
+          <div>
+            <p>M=✓</p>
+            <p>D=May be</p>
+            <p>but folows Scalar addition</p>
+          </div>
+        </div>
+        <div>
+          <p>Vectors</p>
+          <div>
+            <p>M=✓</p>
+            <p>D=✓</p>
+            <p>+ (Vector laws of addition)</p>
+            <p>Addition is θ dependent</p>
+          </div>
+        </div>
+        <div>
+          <SimpleLatex
+            expr={String.raw`\left\{
+\begin{array}{c}
+\overrightarrow{Force} \\
+\overrightarrow{disp} \\
+\overrightarrow{acc}
+\end{array}
+\right\}`}
+          />
+        </div>
+      </div>
+      <div>
+        <p>⊛ Current (does not follow Vector Laws of addition)</p>
+      </div>
     </div>
   );
 };
